379d27956b41c68f4492ced33292fe31ccf7555f
Marcos Mellado <marcos.mellado@akqa.com>
2020-02-04 11:00:17 +0100

    Remove http redirection

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,16 +2,9 @@ exports.createPages = ({ actions }) => {
   const { createRedirect } = actions;
 
   createRedirect({
-    fromPath: "http://mellado.me/*",
-    toPath: "https://marcos.mellado.me/:splat",
-    statusCode: 301,
-    isPermanent: true,
-  });
-
-  createRedirect({
     fromPath: "https://mellado.me/*",
     toPath: "https://marcos.mellado.me/:splat",
-    statusCode: 301,
+    statusCode: "301!",
     isPermanent: true,
   });
 };
